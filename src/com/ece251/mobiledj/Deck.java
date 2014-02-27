f5e7bf0be1fb75f180cee05d7fefbc848f25ab1c
Charles Crain <craincharles@gmail.com>
2014-02-26 18:24:36 -0800

    I began my class for Record_PlayBack and I beleive it is finished. I made 2 new methods for initializing the buttons to be called on in onCreate. .

--- src/com/ece251/mobiledj/Deck.java
+++ src/com/ece251/mobiledj/Deck.java
@@ -4,8 +4,15 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
 public class Deck extends Activity {
+	Record_PlayBack rec1, rec2, rec3, rec4;
+	Button record1;
+	Button record2;
+	Button record3;
+	Button record4;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +20,13 @@ public class Deck extends Activity {
 		setContentView(R.layout.activity_deck);
 		
 		Log.d("For Fun", "Testing Git");
+		
+		initialize_record();
+		
+		
+		
+		
+		
 	}
 	//TEST
 
@@ -25,5 +39,22 @@ public class Deck extends Activity {
 
 	//TEST TESTEST
 	
+	private void initialize_record(){
+		rec1 = new Record_PlayBack("recording1.pcm");
+		rec2 = new Record_PlayBack("recording2.pcm");
+		rec3 = new Record_PlayBack("recording3.pcm");
+		rec4 = new Record_PlayBack("recording4.pcm");
+		
+	}
 	
+	private void init_Button(){
+		record1  = (Button) findViewById(R.id.Record_button1);
+		record2  = (Button) findViewById(R.id.Record_button2);
+		record3  = (Button) findViewById(R.id.Record_button3);
+		record4  = (Button) findViewById(R.id.Record_button4);
+		
+		
+		
+		
+	}
 }
---
+++ src/com/ece251/mobiledj/Record_PlayBack.java
@@ -0,0 +1,121 @@
+package com.ece251.mobiledj;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+import android.os.Environment;
+
+public class Record_PlayBack {
+	
+	private String filename;
+	private File file;
+	
+	
+	public boolean recording;
+	
+	public Record_PlayBack(String name){
+		filename = name;
+        file = new File(Environment.getExternalStorageDirectory(), name);  //make a new file
+		
+	}
+	
+	public void record(){
+		try
+        {
+              file.createNewFile();  //do a try to make sure making new file works
+              
+              //Set newly made file to a DataOutputStream
+              OutputStream outputStream = new FileOutputStream(file);
+              BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+              DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream);
+
+              int minBufferSize = AudioRecord.getMinBufferSize(11025,
+                          AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                          AudioFormat.ENCODING_PCM_16BIT);
+
+              short[] audioData = new short[minBufferSize];
+
+              AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                          11025,
+                          AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                          AudioFormat.ENCODING_PCM_16BIT,
+                          minBufferSize);
+
+              audioRecord.startRecording();
+
+              while(recording)
+              {
+                    int numberOfShort = audioRecord.read(audioData, 0, minBufferSize);
+                    for(int i = 0; i < numberOfShort; i++)
+                    {
+                          dataOutputStream.writeShort(audioData[i]);
+                    }
+              }
+              audioRecord.stop();
+              dataOutputStream.close();
+
+        }
+        catch (IOException e)
+        {
+              e.printStackTrace();
+        }
+	}
+	
+	public void play(){
+          int shortSizeInBytes = Short.SIZE/Byte.SIZE;
+
+          int bufferSizeInBytes = (int)(file.length()/shortSizeInBytes);
+          short[] audioData = new short[bufferSizeInBytes];
+
+          try {
+                InputStream inputStream = new FileInputStream(file);
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+                DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
+
+                int i = 0;
+                while(dataInputStream.available() > 0)
+                {
+                      audioData[i] = dataInputStream.readShort();
+                      i++;
+                }
+
+                dataInputStream.close();
+
+                AudioTrack audioTrack = new AudioTrack(
+                            AudioManager.STREAM_MUSIC,
+                            11025,
+                            AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                            AudioFormat.ENCODING_PCM_16BIT,
+                            bufferSizeInBytes,
+                            AudioTrack.MODE_STREAM);
+                audioTrack.setStereoVolume(AudioTrack.getMaxVolume(), AudioTrack.getMaxVolume());
+                audioTrack.play();
+                audioTrack.write(audioData, 0, bufferSizeInBytes);
+
+
+          } catch (FileNotFoundException e)
+          {
+                e.printStackTrace();
+          } catch (IOException e)
+          {
+                e.printStackTrace();
+          }
+    } 
+	
+	
+
+}
